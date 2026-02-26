dedd9e528599d9b55c3fce03f949666e68c9ef6b
Kirthi JC <its.me.jckirthi@gmail.com>
2026-02-26 11:02:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Swap The Numbers/swap-the-numbers.java
+++ Difficulty: Basic/Swap The Numbers/swap-the-numbers.java
@@ -1,13 +1,17 @@
 // User function Template for Java
+import java.io.*;
+import java.util.*;
 
-class Solution {
-    public void swap(int a, int b) {
-        // code here
-        int temp=0;
-        temp=a;
-        a=b;
-        b=temp;
-
+class GFG {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        // Write Code to Swap
+int temp=0;
+temp=a;
+a=b;
+b=temp;
         System.out.println(a + " " + b);
     }
 }
