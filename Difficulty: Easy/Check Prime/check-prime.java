8c550d17500459e545893d54b2204f20fb6ee2dd
Kirthi JC <its.me.jckirthi@gmail.com>
2026-03-01 14:18:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Check Prime/check-prime.java
@@ -0,0 +1,13 @@
+class Solution {
+    public static boolean prime(int n) {
+        // Write your code here
+        if(n==1 || n==0)
+        return false;
+        for(int i=2;i*i<=n;i++)
+        {
+            if(n%i==0)
+            return false;
+        }
+        return true;
+    }
+}
